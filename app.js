81c31a45fb611a836e95c8b47336c63c3c443a77
EgorStogov <egst94@yandex.ru>
2022-06-24 02:10:07 +0300

    feature: add schemas & models

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const app = express();
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-})
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, _res, next) => {
+  req.user = {
+    _id: '62b4a23d553c410c9aeb3a68'
+  };
+
+  next();
+});
+app.use('', userRouter);
+app.use('', cardRouter);
+
+mongoose.connect('mongodb://localhost:27017/mestodb')
 
 app.listen(PORT, () => {
   console.log("it's alive")
---
+++ controllers/users.js
@@ -0,0 +1,78 @@
+const User = require('../models/user');
+
+const getUsers = (_req, res) => {
+  User.find({})
+  .then(users => res.send({data: users}))
+  .catch((err) => res.status(500).send({ message: 'Что-то пошло не так' }));
+}
+
+const getUser = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+  .then(user => {
+    if (!user) {
+      return res.status(404).send({ message: 'Пользователь не найден' })
+    }
+    return res.send({data: user});
+  })
+  .catch((_) => res.status(500).send({ message: 'Что-то пошло не так' }))
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+  .then(newUser => res.send({ data: newUser }))
+  .catch((err) => {
+    if (err.name === 'ValidationError') {
+      res.status(400).send({ message: 'Введены некорректные данные' });
+    } else {
+      res.status(500).send({ message: 'Что-то пошло не так' });
+    }
+  })
+}
+
+const updateUserProfile = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true }
+  )
+  .then(updatedUser => {
+    if (!updatedUser) {
+      return res.status(404).send({ message: 'Пользователь не найден' });
+    }
+    return res.send({ data: updatedUser });
+  })
+  .catch((err) => {
+    if (err.name === 'ValidationError') {
+      res.status(400).send({message: 'Введены некорректные данные'});
+    } else {
+      res.status(500).send({message: 'Что-то пошло не так'});
+    }
+  })
+}
+
+const updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true }
+  )
+  .then(updatedUser => {
+    if (!updatedUser) {
+      return res.status(404).send({ message: 'Пользователь не найден' });
+    }
+    res.send({ data: updatedUser })
+  })
+  .catch((err) => {
+    if (err.name === 'ValidationError') {
+      res.status(400).send({ message: 'Введены некорректные данные' })
+    } else {
+      res.status(500).send({ message: 'Что-то пошло не так' })
+    }
+  })
+}
+
+module.exports = { getUsers, getUser, createUser, updateUserProfile, updateUserAvatar };
