008ffb38dbcc0bd43692a971c4ceb7e2458a5bfb
EgorStogov <egst94@yandex.ru>
2022-06-26 12:28:10 +0300

    feature: add middleware for authorization

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
+const auth = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
@@ -22,10 +23,13 @@ app.use((req, _res, next) => {
 
   next();
 });
-app.use('/users', userRouter);
-app.use('/cards', cardRouter);
 app.post('/signin', login);
 app.post('/signup', createUser);
+
+app.use(auth);
+
+app.use('/users', userRouter);
+app.use('/cards', cardRouter);
 
 app.use('*', (_req, res) => {
   res.status(404).send({ message: 'Страница не найдена' });
