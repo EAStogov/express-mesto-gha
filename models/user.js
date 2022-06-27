0fcccb139a29ce198e06feefed04fa258633c64c
EgorStogov <egst94@yandex.ru>
2022-06-27 19:43:20 +0300

    refactor: refactor error 29

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-// const UnauthorizedError = require('../errors/UnauthorizedError');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -38,15 +38,21 @@ const userSchema = new mongoose.Schema({
 });
 
 // eslint-disable-next-line func-names
-userSchema.statics.findUserByCredentials = function (email, password) {
+userSchema.statics.findUserByCredentials = function (email, password, next) {
   return this.findOne({ email }).select('+password')
-    .then((user) => bcrypt.compare(password, user.password)
-      .then((matched) => {
-        if (matched) {
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError('Неправильные почта или пароль');
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new UnauthorizedError('Неправильные почта или пароль');
+          }
           return user;
-        }
-        return {};
-      }));
+        })
+        .catch(next);
+    });
 };
 
 module.exports = mongoose.model('user', userSchema);
