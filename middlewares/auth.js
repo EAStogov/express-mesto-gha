b118aa44160299151c2c5b57c6bb3b0617181dcd
EgorStogov <egst94@yandex.ru>
2022-06-27 22:16:37 +0300

    refactor: refactor error 34

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -13,7 +13,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, process.env.JWT_SECRET);
+    payload = jwt.verify(token, 'd92f6c25c8bd88007924dd817ca05574');
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
