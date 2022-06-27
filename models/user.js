dfda74c138371fcdcd2a57298cf6f3f8d3c323aa
EgorStogov <egst94@yandex.ru>
2022-06-27 16:02:53 +0300

    refactor: refactor error 22

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const { isEmail } = require('validator');
 
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
