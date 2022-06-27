d3b17388fcfd75c1a57952cf5cee135973257f4b
EgorStogov <egst94@yandex.ru>
2022-06-27 12:53:33 +0300

    refactor: refactor error 12

--- models/user.js
+++ models/user.js
@@ -30,11 +30,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    validate: {
-      validator(v) {
-        return isEmail(v);
-      },
-    },
   },
   password: {
     type: String,
