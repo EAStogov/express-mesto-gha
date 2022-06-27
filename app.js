38e93221fa332e28fa91c8f327747faa48aa124d
EgorStogov <egst94@yandex.ru>
2022-06-27 15:53:49 +0300

    refactor: refactor error 21

--- app.js
+++ app.js
@@ -29,9 +29,6 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required(),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().domain(),
   }).unknown(true),
 }), createUser);
 
